e0dbbac10c9dffcae5963262a74ed028961eafcd
Felix Mohrmann <mohrmann.felix@gmail.com>
2019-07-03 09:18:39 +1000

    Added JLabel and reordered the structure

--- main/java/ClickKlim/Main.java
+++ main/java/ClickKlim/Main.java
@@ -1,23 +1,46 @@
 package ClickKlim;
 
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public final class Main{
 
-    private static final int WIDTH = 1920;
-    private static final int HEIGHT = 1080;
-
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 500;
+    
     public static void main(String[] args) {
-    	GameValues gameData = new GameValues();
-    	Display display = new Display();
-        JFrame frame = new JFrame();
+        // Display display = new Display();
+        // GameValues gameData = new GameValues();
+        createWindow();
+    }
+
+    static void createWindow(){
+        
+        //Create panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        //Build the frame
+        JFrame frame = new JFrame("Window");
         frame.setSize(new Dimension(WIDTH, HEIGHT));
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.add(display);
+
+        //Create button
+        // JButton button = new JButton("I am a button");
+
+        //Create label
+        JLabel label = new JLabel("Milk: ");
+
+        //Add label and button to panel
+        panel.add(label);
+        // panel.add(button);
+
+        //Add panel to frame
+        frame.add(panel);
+
+        //Make the frame visible
         frame.setVisible(true);
     }
 }
