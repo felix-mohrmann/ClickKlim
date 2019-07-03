820d394c02f43f4f6866e3a6eae89ce3aacf4859
Felix Mohrmann <mohrmann.felix@gmail.com>
2019-07-03 13:47:04 +1000

    set up basic panels

--- main/java/ClickKlim/Main.java
+++ main/java/ClickKlim/Main.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 
 public final class Main{
 
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 500;
+    private static final int WIDTH = 1250;
+    private static final int HEIGHT = 750;
     
     public static void main(String[] args) {
         createWindow();
@@ -15,17 +15,21 @@ public final class Main{
     static void createWindow(){
         GameValues gameData = new GameValues();
 
-        //Create the frame
+        // Create the frame
         JFrame frame = new JFrame("ClickKlim");
         frame.setSize(new Dimension(WIDTH, HEIGHT));
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setLayout(new FlowLayout());
 
-        Display milkDisplay = new Display(gameData);
-
-        //Add the display to the frame and make it visible
-        frame.add(milkDisplay);
+        // Create the Panel's
+        Display milkPanel = new Display("Milk", gameData.getMilk());
+        Display storagePanel = new Display("Storage", gameData.getStorage());
+        
+        // Add the Panel's to the frame and make the frame visible
+        frame.add(milkPanel);
+        frame.add(storagePanel);
         frame.setVisible(true);
     }
 }
