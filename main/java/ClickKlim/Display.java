13bc509c8ee8e60775508a63c80838320fa20df0
Felix Mohrmann <mohrmann.felix@gmail.com>
2019-08-22 19:00:47 +1000

    added milk label

--- main/java/ClickKlim/Display.java
+++ main/java/ClickKlim/Display.java
@@ -3,6 +3,7 @@ package ClickKlim;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Display extends JPanel {
@@ -12,9 +13,10 @@ public class Display extends JPanel {
     private static final int HEIGHT = 750;
 
     private static ArrayList<JLabel> labels = new ArrayList<JLabel>();
-
+    ImageIcon milkPic =  new ImageIcon("main/images/milk.jpg");
+    
     public Display (GameValues data) { 
-        createFrame(data);
+        createFrame(data); 
     }
 
     public void createFrame(GameValues data) {
@@ -68,20 +70,29 @@ public class Display extends JPanel {
         createLabel("Storage", data.getStorage());
         
         // Add everything to the frame and make it visible
+        // REWRITE: make a for-loop out of this!
         frame.setJMenuBar(menubar);
         frame.add(labels.get(0));
         frame.add(labels.get(1));
+        frame.add(labels.get(2));
         frame.setVisible(true);
     }
 
     public void createLabel(String type, double value) {
-        JLabel label = new JLabel(type + ": " + value);		//Create label
-        this.add(label);		//Add label to panel
-        labels.add(label);
+        JLabel label = new JLabel(type + ": " + value);     //Create label    
+        labels.add(label);  //Add label to labels-array
+        labels.add(createMilkButton());
     }
     
-    public void updateLabel() {
+    void updateLabel() {
     	labels.get(0).setText("Milk: " + Main.getGameData().getMilk());
-    	labels.get(1).setText("Storage: " + Main.getGameData().getStorage());
+        labels.get(1).setText("Storage: " + Main.getGameData().getStorage());
+        labels.get(2).setText("");
+    }
+
+    public JLabel createMilkButton(){
+        JLabel milkLabel = new JLabel();
+        milkLabel.setIcon(milkPic);
+        return milkLabel;
     }
 }
